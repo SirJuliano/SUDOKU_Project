ba8486bb086bb52293a09c64115d8e322863050b
SirJuliano <130677407+SirJuliano@users.noreply.github.com>
2023-11-18 13:53:42 +0000

    State All Changes

--- SUDOKU/Notes.h
+++ SUDOKU/Notes.h
@@ -2,22 +2,20 @@
 #define SUDOKU_PROJECT_ERREUR_ANTOINE_NOTES_H
 #include "Grille.h"
 
-void initNote(T_grid grid, int length);
-void setNote1(Box cell, int numNote);
-void setNote1_v2(int *num, int numNote);
+// void initNote(T_grid grid, int length);
+void setNote1(Box *cell, int numNote);                                  //Met à 1 le bit "numNote" de la note contenue dans "*cell"
+void setNote0(Box *cell, int numNote);                                  //Met à 0 le bit "numNote" de la note contenue dans "*cell"
+void setNote0Line(T_grid grid, int x, int y, int length, int numNote);  //Met à 0 le bit "numNote" des notes de toutes les cases de la ligne
+void setNote0Column(T_grid grid, int x, int y, int length, int numNote);//Met à 0 le bit "numNote" des notes de toutes les cases de la colonne
+void setNote0Square(T_grid grid, int x, int y, int length, int numNote);//Met à 0 le bit "numNote" des notes de toutes les cases du bloc
+void setNote0Zone(T_grid grid, int x, int y,int length, int numNote);   //Applique ensembles les 3 précédentes fonctions
 
-void setNote0(Box cell, int numNote);
-void setNote0_v2(int *cell, int numNote);
+// int getNote(Box *cell, int numNote);
+// void printBinary2(Box cell);
+// void printBinary(int num);
 
-void setNote0Line(T_grid grid, int x, int y, int length, int numNote);
-void setNote0Column(T_grid grid, int x, int y, int length, int numNote);
-void setNote0Square(T_grid grid, int x, int y, int length, int numNote);
-void setVal(T_grid grid, int x, int y,int length, int numNote);
-int getNote(Box *cell, int numNote);
-void printBinary2(Box cell);
-void printBinary(int num);
-
-int getvalNote(int note);
-bool oneNoteLeft (int note);
-
-#endif //SUDOKU_PROJECT_ERREUR_ANTOINE_NOTES_H
+int getvalNote(int note);                                               //renvois la version décimal de la note
+bool oneNoteLeft (int note);                            
+        
+    //verifie qu'il n'y ai plus qu'une valeur possible sur une case#endif //SUDOKU_PROJECT_ERREUR_ANTOINE_NOTES_H
+#endif
